287c3fcf7978d1eeda48d95c1212f838ed6ee6b6
Kauan Sano <kauansano0@gmail.com>
2023-06-16 18:14:15 -0300

    primeiras mudanças e rotas. início do projeto

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const router = require('index')
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require("./routes/index"))
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`app listening on ${process.env.APP_PORT}`);
+})
