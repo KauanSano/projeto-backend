92be7b0329407fa8cf45f81d776b47c800e36a29
Kauan Sano <kauansano0@gmail.com>
2023-06-27 15:52:03 -0300

    mvc

--- model/types.js
+++ model/types.js
@@ -21,37 +21,5 @@ const TypesModel = sequelize.define("Types", {
 });
 
 module.exports = {
-  list: async function () {
-    const Type = await TypesModel.findAll();
-    return Type;
-  },
-  save: async function (name) {
-    try {
-      const Type = await TypesModel.create({
-        name: name,
-      });
-      return Type;
-    } catch (e) {
-      console.log(`Houve um erro ao tentar salvar o tipo: ${e}`);
-      return null;
-    }
-  },
-  getIdByName: async function (name) {
-    try {
-      var Type = await TypesModel.findOne({ where: { name: name } });
-      return Type.id;
-    } catch (e) {
-      return e.message;
-    }
-  },
-  getById: async function (id) {
-    var obj = await TypesModel.findOne({ where: { id: id } });
-    if (obj === null) {
-      console.log("Não foi possível achar o objeto de TIPO pelo ID. ");
-      return null;
-    } else {
-      return obj;
-    }
-  },
   Model: TypesModel,
 };
